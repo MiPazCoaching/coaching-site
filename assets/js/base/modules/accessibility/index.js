35eda247f57824b08b8f62c03407cdeea6012a3b
ChispiDEV <cristinafg93@hotmail.com>
2025-07-08 20:17:21 +0200

    Update index.js

--- assets/js/base/modules/accessibility/index.js
+++ assets/js/base/modules/accessibility/index.js
@@ -2,10 +2,8 @@
 
 import { initAccessibilitySidebar } from './accessibility-sidebar.js';
 import { initAccessibilityPopup } from './accessibility-popup.js';
-import { initAccessibilityState } from './accessibility-state.js';
 
 export function initAccessibilityFeatures() {
   initAccessibilitySidebar();
   initAccessibilityPopup();
-  initAccessibilityState();
 }
